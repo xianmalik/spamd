2fc93820c72bf57a40859b8383b4bde845ba309d
Zubayer <haider15-5902@diu.edu.bd>
2017-05-25 22:00:02 +0600

    Fixed

--- js/script.js
+++ js/script.js
@@ -1,18 +1,18 @@
 jQuery(document).ready(function($){
     $('.before-after').queue(function(){
-        var $after = $(this).find('.after'),
-    		img_height = $(this).find('.after img').height(),
+        var $before = $(this).find('.before'),
+    		img_height = $(this).find('.before img').height(),
     		init_split = Math.round(img_height/2);
-        $after.height(init_split);
+        $before.height(init_split);
         $(this).height(img_height);
         $(this).find('.ba-slider').css('margin-top', -.5 * $(this).find('.ba-slider').height());
 		$(this).mousemove(function(e){
-            var offX  = (e.offsetY || e.clientY - $after.offset().top);
-            $after.height(offX);
+            var offX  = (e.offsetY || e.clientY - $before.offset().top);
+            $before.height(offX);
             $(this).find('.ba-slider').css('top', offX);
 		});
 		$(this).mouseleave(function(e){
-            $after.stop().animate({ height: init_split },1000);
+            $before.stop().animate({ height: init_split },1000);
             $(this).find('.ba-slider').css('top', init_split);
         });
     });
