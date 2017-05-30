e0b7f8acb23af5b7d4539f1f76bb9efba9338fdb
Zubayer <haider15-5902@diu.edu.bd>
2017-05-30 23:13:07 +0600

    Done

--- js/script.js
+++ js/script.js
@@ -1,19 +1,8 @@
-jQuery(document).ready(function($){
-    $('.before-after').queue(function(){
-        var $before = $(this).find('.before'),
-    		img_height = $(this).find('.before img').height(),
-    		init_split = Math.round(img_height/2);
-        $before.height(init_split);
-        $(this).height(img_height);
-        $(this).find('.ba-slider').css('margin-top', -.5 * $(this).find('.ba-slider').height());
-		$(this).mousemove(function(e){
-            var offX  = (e.offsetY || e.clientY - $before.offset().top);
-            $before.height(offX);
-            $(this).find('.ba-slider').css('top', offX);
-		});
-		$(this).mouseleave(function(e){
-            $before.stop().animate({ height: init_split },1000);
-            $(this).find('.ba-slider').css('top', init_split);
-        });
-    });
+document.querySelectorAll('.cocoen').forEach(function(element){
+  new Cocoen(element);
+});
+$('ul.nav li.dropdown').hover(function() {
+  $(this).find('.dropdown-menu').stop(true, true).fadeIn(500);
+}, function() {
+  $(this).find('.dropdown-menu').stop(true, true).fadeOut(500);
 });
